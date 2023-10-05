0c676aec2c320102a941dfa4c4baa8d3d5eec4b2
“Neumoin <d.neumoin@vk.team>
2023-10-05 14:39:08 +0300

    tmp add

--- src/integration_test/delivery_usecase/friend_test.go
+++ src/integration_test/delivery_usecase/friend_test.go
@@ -1,6 +1,7 @@
 package deliveryusecase
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -26,6 +27,7 @@ type FriendTestIntegrationSuite struct {
 	delivery       *friendDelivery.Delivery
 	friendRepoMock *friendRepoMock.RepositoryI
 	fRelBuilder    *testutils.FriendRelationBuilder
+	userBuilder    *testutils.UserBuilder
 	uRepoMock      *userRepoMock.RepositoryI
 }
 
@@ -38,6 +40,7 @@ func (s *FriendTestIntegrationSuite) BeforeEach(t provider.T) {
 	s.uRepoMock = userRepoMock.NewRepositoryI(t)
 	s.uc = friendUC.New(s.friendRepoMock, s.uRepoMock)
 	s.fRelBuilder = testutils.NewFriendRelationBuilder()
+	s.userBuilder = testutils.NewUserBuilder()
 
 	s.router = echo.New()
 	s.delivery = &friendDelivery.Delivery{
@@ -69,4 +72,113 @@ func (s *FriendTestIntegrationSuite) TestSubscribe(t provider.T) {
 	integrationutils.AuthMiddlewareAction(ctx)
 	t.Require().NoError(s.delivery.Subscribe(ctx))
 	t.Assert().Equal(http.StatusCreated, rec.Code)
+}
+
+func (s *FriendTestIntegrationSuite) TestUnsubscribe(t provider.T) {
+	req := httptest.NewRequest(http.MethodDelete, "/", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := s.router.NewContext(req, rec)
+	ctx.SetPath("/friends/unsubscribe/:user_id")
+	ctx.SetParamNames("user_id")
+	ctx.SetParamValues("2")
+
+	friendRel := s.fRelBuilder.WithSubID(1).WithUserID(2).Build()
+	s.friendRepoMock.On("CheckFriends", mock.MatchedBy(func(arg *models.FriendRelation) bool {
+		return *arg.SubscriberID == *friendRel.SubscriberID && *arg.UserID == *friendRel.UserID
+	})).Return(true, nil)
+
+	s.friendRepoMock.On("DeleteFriendRelation", mock.MatchedBy(func(arg *models.FriendRelation) bool {
+		return *arg.SubscriberID == *friendRel.SubscriberID && *arg.UserID == *friendRel.UserID
+	})).Return(nil)
+
+	// имитируем вызов мидлвары
+	integrationutils.AuthMiddlewareAction(ctx)
+	t.Require().NoError(s.delivery.Unsubscribe(ctx))
+	t.Assert().Equal(http.StatusNoContent, rec.Code)
+}
+
+func (s *FriendTestIntegrationSuite) TestGetUserSubs(t provider.T) {
+	req := httptest.NewRequest(http.MethodDelete, "/", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := s.router.NewContext(req, rec)
+	ctx.SetPath("/user/:user_id/subs")
+	ctx.SetParamNames("user_id")
+	ctx.SetParamValues("1")
+
+	userID := uint64(1)
+	subsIDs := []uint64{2, 3, 4, 5}
+	subs := testutils.BuildUsersByIDs(s.userBuilder, subsIDs)
+
+	s.friendRepoMock.On("GetUserSubs", userID).Return(subsIDs, nil)
+	s.uRepoMock.On("GetUsersByIDs", subsIDs).Return(subs, nil)
+
+	expData := map[string][]map[string]interface{}{
+		"body": {},
+	}
+	bodyArr := []map[string]interface{}{}
+	for idx := range subs {
+		new := map[string]interface{}{
+			"id":    subs[idx].ID,
+			"name":  subs[idx].Name,
+			"email": subs[idx].Email,
+			"about": subs[idx].About,
+			"role":  subs[idx].Role,
+		}
+		bodyArr = append(bodyArr, new)
+	}
+
+	expData["body"] = bodyArr
+	expDataJson, err := json.Marshal(expData)
+	t.Require().NoError(err, "error while prepare expDataJson")
+
+	// имитируем вызов мидлвары
+	integrationutils.AuthMiddlewareAction(ctx)
+	t.Require().NoError(s.delivery.GetUserSubs(ctx))
+	t.Assert().Equal(http.StatusOK, rec.Code)
+	t.Assert().JSONEq(string(expDataJson), rec.Body.String())
+}
+
+func (s *FriendTestIntegrationSuite) TestGetUserFriends(t provider.T) {
+
+	req := httptest.NewRequest(http.MethodDelete, "/", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := s.router.NewContext(req, rec)
+	ctx.SetPath("/user/:user_id/friends")
+	ctx.SetParamNames("user_id")
+	ctx.SetParamValues("1")
+
+	userID := uint64(1)
+	friendIDs := []uint64{2, 3, 4, 5}
+	friends := testutils.BuildUsersByIDs(s.userBuilder, friendIDs)
+
+	s.friendRepoMock.On("GetUserFriends", userID).Return(friendIDs, nil)
+	s.uRepoMock.On("GetUsersByIDs", friendIDs).Return(friends, nil)
+
+	expData := map[string][]map[string]interface{}{
+		"body": {},
+	}
+	bodyArr := []map[string]interface{}{}
+	for idx := range friends {
+		new := map[string]interface{}{
+			"id":    friends[idx].ID,
+			"name":  friends[idx].Name,
+			"email": friends[idx].Email,
+			"about": friends[idx].About,
+			"role":  friends[idx].Role,
+		}
+		bodyArr = append(bodyArr, new)
+	}
+
+	expData["body"] = bodyArr
+	expDataJson, err := json.Marshal(expData)
+	t.Require().NoError(err, "error while prepare expDataJson")
+
+	// имитируем вызов мидлвары
+	integrationutils.AuthMiddlewareAction(ctx)
+	t.Require().NoError(s.delivery.GetUserSubs(ctx))
+	t.Assert().Equal(http.StatusOK, rec.Code)
+	t.Assert().JSONEq(string(expDataJson), rec.Body.String())
 }
